6da8b787dc19a1fe9e62d89272ed7c49c2d94567
Rubiyatno <LOG.Rubiyatno2@infra.core>
2025-06-03 13:46:31 +0700

    tambahan folder utils dam menambahkan hook tugas pertemuan3

---
+++ src/Utils/books.js
@@ -0,0 +1,76 @@
+const books = [
+    {
+        id: 1,
+        title: "Atomic Habits",
+        author: "James Clear",
+        year: 2018,
+        description: "Atomic Habits: Perubahan Kecil yang memberikan hasil luar biasa",
+        image: "https://perpustakaan.politekniktempo.ac.id/images/content/1424023910.jpeg",
+    },
+    {
+        id: 2,
+        title: "Deep Work",
+        author: "Cal Newport",
+        year: 2016,
+        description: "Rahasia untuk sukses dalam dunia yang penuh distraksi.",
+        image: "https://media.suara.com/pictures/653x366/2023/06/18/64182-ilustrasi-buku-deep-work-docpribadi-ezna-artoria.webp"
+      },
+      {
+        id: 3,
+        title: "The Subtle Art of Not Giving a F*ck",
+        author: "Mark Manson",
+        year: 2016,
+        description: "Pendekatan berbeda dalam menghadapi hidup dan kebahagiaan.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg"
+      },
+      {
+        id: 4,
+        title: "Thinking, Fast and Slow",
+        author: "Daniel Kahneman",
+        year: 2011,
+        description: "Penjelasan tentang dua cara berpikir manusia: cepat dan lambat.",
+        image: "https://down-id.img.susercontent.com/file/9ec1376378af2f6f47dd5bee593c658a@resize_w450_nl.webp"
+      },
+      {
+        id: 5,
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        year: 1997,
+        description: "Pelajaran finansial yang tidak diajarkan di sekolah.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg"
+      },
+      {
+        id: 6,
+        title: "Start With Why",
+        author: "Simon Sinek",
+        year: 2009,
+        description: "Pemimpin besar menginspirasi tindakan dengan satu pertanyaan sederhana: Mengapa?",
+        image: "https://images.tokopedia.net/img/cache/700/VqbcmM/2024/3/26/e59ecfcd-0bb4-4d83-8380-8f9b0384fd9a.jpg"
+      },
+      {
+        id: 7,
+        title: "Can't Hurt Me",
+        author: "David Goggins",
+        year: 2018,
+        description: "Kisah inspiratif tentang ketahanan mental dan ketangguhan ekstrem.",
+        image: "https://images.tokopedia.net/img/cache/250-square/VqbcmM/2025/2/13/a7deae10-9785-49ae-a3a3-89f806b18bb9.png"
+      },
+      {
+        id: 8,
+        title: "The 7 Habits of Highly Effective People",
+        author: "Stephen R. Covey",
+        year: 1989,
+        description: "Tujuh kebiasaan untuk pengembangan pribadi dan kesuksesan.",
+        image: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2024/3/6/0dc0b92b-5c88-4d77-a43f-89c55f6f3df6.jpg"
+      },
+      {
+        id: 9,
+        title: "Ikigai",
+        author: "Héctor García & Francesc Miralles",
+        year: 2016,
+        description: "Rahasia panjang umur dan hidup bahagia dari Jepang.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/71tbalAHYCL.jpg"
+      }
+];
+
+export default books;
